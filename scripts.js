e57ff4f48044e94733f61d84d95a41d953a60ed8
Danielle Dong <112914838+danielledong@users.noreply.github.com>
2024-08-23 20:17:43 -0400

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -38,6 +38,14 @@ function submitReview(event) {
     const rating = document.getElementById('rating').value;
     const review = document.getElementById('review').value;
 
+    // Push the rating value to the dataLayer
+    window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({
+        'event': 'reviewSubmission',
+        'reviewerName': name,
+        'rating': rating
+    });
+
     const reviewHTML = `
         <div>
             <h4>${name} (Rating: ${rating}/5)</h4>
@@ -57,10 +65,18 @@ function submitSubscription(event) {
 
     const email = document.getElementById('email').value;
 
+    // Push the email value to the dataLayer
+    window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({
+        'event': 'subscription',
+        'subscriberEmail': email
+    });
+
     alert(`Thank you for subscribing! Discounts will be sent to: ${email}`);
 
     document.getElementById('subscribeForm').reset();
 }
+
 
 function viewAllReviews() {
     const reviewsList = document.getElementById('reviewsList');
